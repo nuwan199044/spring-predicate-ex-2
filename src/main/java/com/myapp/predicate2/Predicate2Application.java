7d4952aef29df3bf6572adfab7c595cd06b88574
Nuwan <nuwan199044@gmail.com>
2024-07-18 21:34:13 +0530

    initial code

---
+++ src/main/java/com/myapp/predicate2/Predicate2Application.java
@@ -0,0 +1,13 @@
+package com.myapp.predicate2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Predicate2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Predicate2Application.class, args);
+	}
+
+}
